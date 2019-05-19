7b4a65c5ccd77cc8f52d58836ed3f89d0f0ac2d1
Amanda Henrique <aamandahenrique@hotmail.com>
2019-05-19 18:23:28 -0300

    Adicionado secao de fatos

--- app/static/script.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector("#menu-responsivo");
-let hamburguer = document.querySelector("#hamburguer");
-
-click = () => {
-    menu.classList.toggle("showMenu")
-}
-
-hamburguer.onclick = click;
