2e4bcb5f613f622be1624c531d563a93455d7f8c
Amanda Henrique <aamandahenrique@hotmail.com>
2019-05-21 16:34:19 -0300

    Adicionado logo da empresa

--- app/static/script.js
+++ app/static/script.js
@@ -3,15 +3,26 @@ let inputs = document.querySelector("div#inputs");
 let inputNome = document.querySelector("#id_nome");
 let inputTelefone = document.querySelector("#id_telefone");
 let label = document.querySelectorAll("label");
+let menu = document.querySelector("button");
 
 
+//Adicionando as classses css no formulario gerado pelo django
 form.classList.add("form");
 inputs.classList.add("inputs");
 label[0].classList.add("label");
 label[1].classList.add("label");
-
-
 inputTelefone.setAttribute('placeholder', 'Seu Telefone');
 inputNome.setAttribute('placeholder', 'Seu Nome');
+
+//Scroll do navegador
+
+menu.forEach(item => {
+    console.log("sdsd");
+})
+
+function fu (){
+    console.log('sdsdd')
+}
+
 
 
